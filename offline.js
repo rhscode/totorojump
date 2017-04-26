256d8f3d1e7b7f1b049c1accb39a19f8183271e3
rhscode <rhscodeclub@gmail.com>
2017-04-26 15:41:56 -0400

    Add files via upload

---
+++ offline.js
@@ -0,0 +1,23 @@
+﻿{
+	"version": 1493234691,
+	"fileList": [
+		"data.js",
+		"c2runtime.js",
+		"jquery-2.1.1.min.js",
+		"offlineClient.js",
+		"images/player-sheet0.png",
+		"images/platform-sheet0.png",
+		"images/tiledbackground2.png",
+		"images/tiledbackground5.png",
+		"images/tiledbackground.png",
+		"images/particles.png",
+		"media/videoplayback.m4a",
+		"media/videoplayback.ogg",
+		"icon-16.png",
+		"icon-32.png",
+		"icon-114.png",
+		"icon-128.png",
+		"icon-256.png",
+		"loading-logo.png"
+	]
+}
